2d1f3054f56d2e4e4b845324aa6af0a612dbe097
Wellington Lima <wellington.lima98@hotmail.com>
2024-01-28 15:38:06 -0300

    Update sistemaCadastro.py

--- sistemaCadastro/sistemaCadastro.py
+++ sistemaCadastro/sistemaCadastro.py
@@ -2,11 +2,11 @@
 entrada = input('Você quer "entrar" ou "sair"? ')
 
 #Entrou no sistema e deseja iniciar o cadastro.
-if entrada == 'entrar' or 'Entrar' or 'ENTRAR':
+if entrada == 'entrar' or entrada =='Entrar' or entrada =='ENTRAR':
     print('Você entrou no sistema ')
     #Código para adicionar os dados do cliente e perguntar se deseja adicionar um novo úsuario e sair
 #Deseja sair do sistema.
-elif entrada == 'sair' or 'Sair' or 'SAIR':
+elif (entrada == 'sair' or entrada =='Sair' or entrada == 'SAIR'):
     print('Você saiu do sistema')
 #Opção invalida.
 else:
