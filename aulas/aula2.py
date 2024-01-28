2b7270608487796d51bdbf299f04d5549e607f47
Wellington Lima <wellington.lima98@hotmail.com>
2024-01-28 15:58:21 -0300

    Create aula2.py

---
+++ aulas/aula2.py
@@ -0,0 +1,10 @@
+#Sistema para checar se uma letra qualquer está entre 
+#as letras que estão na string
+
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+    print(f'{encontrar} não está em {nome}')
