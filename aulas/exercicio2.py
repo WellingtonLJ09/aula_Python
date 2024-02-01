603f2faac0ffcadc7674702184960fce14bf17e5
Wellington Lima <wellington.lima98@hotmail.com>
2024-01-31 23:41:07 -0300

    Update exercicio2.py
    Adicionei a solução encontrada para o professor no segundo processo.

--- aulas/exercicio2.py
+++ aulas/exercicio2.py
@@ -26,6 +26,23 @@ if(hora >= 12) and (hora <= 17):
 if(hora >= 18) and (hora <= 23):
     print('Boa noite')
 
+    #RESPOSTA PROFESSOR:
+entrada = input('Digite a hora em números inteiros: ')
+
+try:
+    hora = int(entrada)
+
+    if hora >= 0 and hora <= 11:
+        print('Bom dia')
+    elif hora >= 12 and hora <= 17:
+        print('Bom tarde')
+    elif hora >= 18 and hora <= 23:
+        print('Bom noite')
+    else:
+        print('Não conheço essa hora')
+except:
+    print('Por favor, digite apenas números inteiros')
+
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
