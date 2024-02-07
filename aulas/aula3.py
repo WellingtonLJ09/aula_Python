b7e5de957f1e778ed33a4de6cf0388fc42a9367f
Wellington Lima <wellington.lima98@hotmail.com>
2024-02-06 22:59:48 -0300

    Update aula3.py
    tabuada simples para exemplos de while(enquanto)

--- aulas/aula3.py
+++ aulas/aula3.py
@@ -4,11 +4,15 @@ REPETIÇÕES
 While(enquanto)
 Executa uma ação enquanto uma condição for verdadeira
 '''
-contador = 0
+numero = input('Digite o número que você deseja saber a tabuada: ')
+numero = int(numero)
+i = 0
 
-while contador < 10:
-    contador = contador +1
-    print(contador)
+while i <= 10:
+    soma = numero + i
+    print(f'{i} + {numero} = {soma}')
+    i += 1
     
 
 print("fim da conta")
+
