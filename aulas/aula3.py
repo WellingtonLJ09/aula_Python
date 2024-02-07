2dcb1ae5916a254d2493df9340c2b48f77f2639d
Wellington Lima <wellington.lima98@hotmail.com>
2024-02-06 22:39:31 -0300

    Create aula3.py

---
+++ aulas/aula3.py
@@ -0,0 +1,14 @@
+'''
+REPETIÇÕES
+
+While(enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+'''
+contador = 0
+
+while contador < 10:
+    contador = contador +1
+    print(contador)
+    
+
+print("fim da conta")
