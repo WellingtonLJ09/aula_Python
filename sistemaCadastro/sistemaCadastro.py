4c0e3f20fbc47417005bec7769532b4362f93e79
Wellington Lima <wellington.lima98@hotmail.com>
2024-02-07 00:24:06 -0300

    Update sistemaCadastro.py
    Atualização no sistema, inserido o 'WHLE' e inserido o "lower" e "startswith" para colocar a resposta em letra minuscula e trabalhar com base na primeira letra da resposta.

--- sistemaCadastro/sistemaCadastro.py
+++ sistemaCadastro/sistemaCadastro.py
@@ -3,16 +3,22 @@ um novo úsuario ou sair. Ao informar que deseja cadastrar um novo úsuario o si
 dados como nome, CPF, data de nascimento e telefone, quando terminar de inserir os dados
 pergunta novamente se deseja adicionar um novo usuario ou sair do sistema.'''
 
-#Entrar no sistema de Registro
-entrada = input('Você quer "entrar" ou "sair"? ')
-
-#Entrou no sistema e deseja iniciar o cadastro.
-if entrada == 'entrar' or entrada =='Entrar' or entrada =='ENTRAR':
-    print('Você entrou no sistema ')
-    #Código para adicionar os dados do cliente e perguntar se deseja adicionar um novo úsuario e sair
+#Inicio no sistema de Registro
+decisao = input('Você quer "entrar" ou "sair" no sistema de registro? ')
+# Entrou no sistema e deseja iniciar o cadastro.
+if decisao.lower().startswith('e'):
+  print('Você entrou no sistema ')
+  # Código para adicionar os dados do cliente e perguntar se deseja adicionar um novo usuário e sair
+  while decisao.lower() != 'sair':
+    nome = input('insira o nome: ')
+    CPF = input('Insira o número do CPF: ')
+    data = input('Insira a data de nascimento: ')
+    tel = input('Insira o número de telefone: ')
+    decisao = input('Deseja cadastrar um novo usuário ou sair? ')
+  print('Você saiu do sistema')
 #Deseja sair do sistema.
-elif (entrada == 'sair' or entrada =='Sair' or entrada == 'SAIR'):
-    print('Você saiu do sistema')
+elif decisao.lower().startswith('s'):
+  print('Você saiu do sistema')
 #Opção invalida.
 else:
-    print('Você não digitou nem entrar e nem sair. ')
+  print('Você não digitou nem entrar e nem sair. ')
